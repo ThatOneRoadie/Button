ce0839a6a35b1b94be2213aad7ab3e8fdefc3228
ThatOneRoadie <thatoneroadie+github@gmail.com>
2017-06-15 19:45:00 -0600

    Update theButton.py

--- theButton.py
+++ theButton.py
@@ -4,12 +4,25 @@ import RFM69
 from RFM69registers import *
 import datetime
 import time
+import RPi.GPIO as GPIO
 
-NODE=1
+#Set pin and node!
+pin=21
+NODE=2
 NET=1
 KEY="Something16chars"
+msg = "Pressed"
 TIMEOUT=3
 TOSLEEP=0.1
+# Use the Broadcom SOC Pin numbers  
+# Setup the Button Pin with Internal pullups enabled and PIN in reading mode.  
+GPIO.setmode(GPIO.BCM)  
+GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
+
+def Transmit(channel):
+	print "Button pressed. Transmitting: " + msg
+    	if radio.sendWithRetry(1, msg, 3, 20):
+        	print "Ack recieved"
 
 radio = RFM69.RFM69(RF69_915MHZ, NODE, NET, True)
 print "class initialized"
@@ -31,35 +44,37 @@ print radio.readTemperature(0)
 print "setting encryption"
 radio.encrypt(KEY)
 
-print "starting loop..."
+# Add Falling Edge Callback
+GPIO.add_event_detect(pin, GPIO.FALLING, callback = Transmit, bouncetime = 2000) 
+
+print "starting loop to wait"
 sequence = 0
-while True:
-
-    msg = "I'm radio %d: %d" % (NODE, sequence)
-    sequence = sequence + 1
-
-    print "tx to radio 2: " + msg
-    if radio.sendWithRetry(2, msg, 3, 20):
-        print "ack recieved"
-
-    print "start recv..."
-    radio.receiveBegin()
-    timedOut=0
-    while not radio.receiveDone():
-        timedOut+=TOSLEEP
-        time.sleep(TOSLEEP)
-	if timedOut > TIMEOUT:
-            print "timed out waiting for recv"
-            break
-
-    print "end recv..."
-    print " *** %s from %s RSSI:%s" % ("".join([chr(letter) for letter in radio.DATA]), radio.SENDERID, radio.RSSI)
-
-    if radio.ACKRequested():
-        print "sending ack..."
-        radio.sendACK()
-    else:
-        print "ack not requested..."
-
-print "shutting down"
-radio.shutdown()
+# Now wait!  
+while 1:  
+    time.sleep(1)  
+#while True:
+#
+#   
+#   sequence = sequence + 1
+#
+#   print "start recv..."
+#   radio.receiveBegin()
+#  timedOut=0
+#   while not radio.receiveDone():
+#       timedOut+=TOSLEEP
+#       time.sleep(TOSLEEP)
+#	if timedOut > TIMEOUT:
+#            print "timed out waiting for recv"
+#            break
+#
+#    print "end recv..."
+#    print " *** %s from %s RSSI:%s" % ("".join([chr(letter) for letter in radio.DATA]), radio.SENDERID, radio.RSSI)
+#
+#    if radio.ACKRequested():
+#        print "sending ack..."
+#        radio.sendACK()
+#    else:
+#        print "ack not requested..."
+#
+#print "shutting down"
+#radio.shutdown()
